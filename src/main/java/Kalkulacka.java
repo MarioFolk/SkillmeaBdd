f6db8597556333903c9e1660be849deb4bdf1a07
Mario Folk <folkmario@gmail.com>
2025-11-11 01:01:08 +0100

    Pridanie kalkulacky

---
+++ src/main/java/Kalkulacka.java
@@ -0,0 +1,30 @@
+import io.cucumber.java.PendingException;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Kalkulacka {
+
+    private int prvCislo;
+    private int druCislo;
+    private int vypocitanyVysledok;
+
+    @Given("Uzivatel ma zadane dve cisla {int} a {int}")
+    public void uzivatelMaZadaneDveCislaA(int prveCislo, int druheCislo) {
+        prvCislo = prveCislo;
+        druCislo = druheCislo;
+
+    }
+
+    @When("Uzivatel spocita tieto dva cisla")
+    public void uzivatelSpocitaTietoDvaCisla() {
+        vypocitanyVysledok = prvCislo + druCislo;
+
+    }
+
+    @Then("Uzivatel vidi na kalkulacke vysledok {int}")
+    public void uzivatelVidiNaKalkulackeVysledok(int predpokladanyVysledok) {
+        assertEquals(predpokladanyVysledok, vypocitanyVysledok);
+    }
+}
