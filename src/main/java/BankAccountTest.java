f57449661aebfc942ef4f3d53ac565fe03e60f82
Mario Folk <folkmario@gmail.com>
2025-11-13 02:18:37 +0100

    Pridanie dalsich krokov bankoveho uctu

--- src/main/java/BankAccountTest.java
+++ src/main/java/BankAccountTest.java
@@ -1,16 +1,31 @@
 import io.cucumber.java.PendingException;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import product.BankAccount;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BankAccountTest {
-    @Given("Uzivatel ma bankovy ucet s {int}OO kc")
-    public void uzivatelMaBankovyUcetSOOKc(int pociatocnyZostatokNaUcte) {
-        BankAccount prvyBankovyUcet = new BankAccount();
+    BankAccount prvyBankovyUcet;
+
+    @Given("Uzivatel ma bankovy ucet s {int} kc")
+    public void uzivatelMaBankovyUcetSKc(int pociatoocnyZostatokNaUcte) {
+      prvyBankovyUcet = new BankAccount(pociatoocnyZostatokNaUcte);
     }
 
     @Then("Na uzivatelskom ucte je {int} kc")
     public void naUzivatelskomUcteJeKc(int predpokladanyZostatokNaUcte) {
+        assertEquals(predpokladanyZostatokNaUcte, prvyBankovyUcet.getAccountBalance(), "test spadol");
+    }
+
+    @When("Uzivatel si na tento ucet prevedie {int} kc")
+    public void uzivatelSiNaTentoUcetPrevedieKc(int ciastkaKPrevodu) {
+
+    }
+
+    @And("Uzivatel si z tohto uctu vyberie {int} kc")
+    public void uzivatelSiZTohtoUctuVyberieKc(int ciastkaKVyberu) {
 
     }
 }
--- src/main/resources/Bank.feature
+++ src/main/resources/Bank.feature
@@ -1,5 +1,11 @@
 Feature: Bank
 
   Scenario: Zostatok na ucte
-    Given Uzivatel ma bankovy ucet s 10OO kc
+    Given Uzivatel ma bankovy ucet s 1000 kc
     Then Na uzivatelskom ucte je 1000 kc
+
+  Scenario: Ucet prevod a vyber penazi
+    Given Uzivatel ma bankovy ucet s 1500 kc
+    When Uzivatel si na tento ucet prevedie 1000 kc
+    And Uzivatel si z tohto uctu vyberie 2000 kc
+    Then Na uzivatelskom ucte je 500 kc
