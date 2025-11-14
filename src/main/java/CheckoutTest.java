b64d4eb78a08518467a296c8f373a47a9eb372d1
Mario Folk <folkmario@gmail.com>
2025-11-14 18:29:13 +0100

    Novy produkt Checkout

--- src/main/java/CheckoutTest.java
+++ src/main/java/CheckoutTest.java
@@ -1,20 +1,30 @@
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.jupiter.api.Assertions;
+import product.Checkout;
 
 public class CheckoutTest {
-    @Given("Cena {string} je {int} kc")
-    public void cenaJeKc(String arg0, int arg1) {
+    Checkout nakupnyKosik;
 
+    @Before
+    public void totoSaPrevediePredKazdymTestom(){
+        nakupnyKosik = new Checkout();
+    }
+
+    @Given("Cena {string} je {int} kc")
+    public void cenaJeKc(String menoProduktu, int cenaProduktu) {
+        nakupnyKosik.addItem(menoProduktu, cenaProduktu);
     }
 
     @When("Uzivatel nakupi {string} v pocte {int} ks")
-    public void uzivatelNakupiVPocteKs(String arg0, int arg1) {
-
+    public void uzivatelNakupiVPocteKs(String menoProduktu, int pocetKusov) {
+        nakupnyKosik.scanItems(menoProduktu, pocetKusov);
     }
 
     @Then("Celkova cena bude {int} kc")
-    public void celkovaCenaBudeKc(int arg0) {
-
+    public void celkovaCenaBudeKc(int predpokladanaCena) {
+        Assertions.assertEquals(predpokladanaCena, nakupnyKosik.totalCart());
     }
 }
--- src/main/resources/Checkout.feature
+++ src/main/resources/Checkout.feature
@@ -4,3 +4,10 @@ Feature: Checkout
     Given Cena "banany" je 10 kc
     When Uzivatel nakupi "banany" v pocte 5 ks
     Then Celkova cena bude 50 kc
+
+  Scenario: Nakup bananov a jablk
+    Given Cena "banany" je 10 kc
+    And Cena "jablk" je 5 kc
+    When Uzivatel nakupi "banany" v pocte 5 ks
+    And Uzivatel nakupi "jablk" v pocte 2 ks
+    Then Celkova cena bude 60 kc
